885b873a893b934ee32d8acb5c6967e22d475732
Komal <guptakomal9950@gmail.com>
2022-08-01 10:51:02 +0530

    First Commit

---
+++ src/main/java/komalgupta1/TestMaven/App.java
@@ -0,0 +1,13 @@
+package komalgupta1.TestMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/komalgupta1/TestMaven/SeleniumTest.java
@@ -0,0 +1,20 @@
+package komalgupta1.TestMaven;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementUI()
+	{
+		System.out.println("elementUI");
+	}
+
+}
